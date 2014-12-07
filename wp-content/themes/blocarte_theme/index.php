c550359de141dec1cb8fee31ebf67a7b247ca170
Nathan Delavictoire <nathan.delavictoire@gmail.com>
2014-12-07 22:41:30 +0100

    Quelques modification du themes on été faites

--- wp-content/themes/blocarte_theme/index.php
+++ wp-content/themes/blocarte_theme/index.php
@@ -3,11 +3,21 @@
 
 <div id="main">
     <div id="content">
-        <h1>Main Area</h1>
-        
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <div class="row">
-            <div class="small-9 large-9 columns">
+        <div class="row">
+            <div class="small-12 large-12 columns panel top">
+                    <h1>Main Area</h1>
+
+            </div>
+            <div class="article small-7 large-7 columns">
+
+
+
+                
+
+
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+
                 <div class="panel">
 
                     <h1><?php the_title(); ?></h1>
@@ -16,22 +26,28 @@
 
                     <?php the_content(__('(more...)')); ?>
                 </div>
-            </div>
-            <hr> <?php endwhile; else: ?>
-            <div class="small-9 large-9 columns">
+
+            <?php endwhile; else: ?>
+
+
+            <div class="panel">
                 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
             </div>
-            
+
+
         </div>
-   
+        <div class="small-1 large-1 columns">
+        </div>
+
+        <!--
+        <div class="small-4 large-4 columns">
+            <div class="panel">
+            <?php get_sidebar(); ?>
+        </div>
+    -->
+
+        
     </div>
-
-    <div class="small-3 large-3 columns">
-                <?php get_sidebar(); ?>
-
-    </div>
-    
-
 </div>
 <div id="delimiter">
 </div>
