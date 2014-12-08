05782412249fd5068bf5d8c04067df6302923777
Nathan Delavictoire <nathan.delavictoire@gmail.com>
2014-12-08 16:50:24 +0100

    Supression de fichier inutile dans les theme. Ajout du dossier pour le theme c-art

--- wp-content/themes/blocarte_theme/footer.php
+++ wp-content/themes/blocarte_theme/footer.php
@@ -1,7 +1,3 @@
-<div id="footer">
+
 	<?php wp_footer(); ?>
 	<h1>FOOTER</h1>
-</div>
-</div>
-</body>
-</html>
--- wp-content/themes/blocarte_theme/index.php
+++ wp-content/themes/blocarte_theme/index.php
@@ -1,42 +1,54 @@
-<?php get_header(); ?>
-
-
-<div id="main">
-    <div id="content">
-        <div class="row">
-            <div class="small-12 large-12 columns panel top">
-                    <h1>Main Area</h1>
-
-            </div>
-            <div class="article small-7 large-7 columns">
-
-
-
-                
-
-
-                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-
-                <div class="panel">
-
-                    <h1><?php the_title(); ?></h1>
-
-                    <h4>Posted on <?php the_time('F jS, Y') ?></h4>
-
-                    <?php the_content(__('(more...)')); ?>
-                </div>
-
-            <?php endwhile; else: ?>
-
-
-            <div class="panel">
-                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-            </div>
-
-
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="wp-content/themes/blocarte_theme/css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <!-- Inclusion of the content of header -->
+            <?php include 'header.php'; ?>
+            <?php get_header(); ?>
         </div>
-        <div class="small-1 large-1 columns">
+
+
+        <div id="main">
+            <div id="content">
+                <div class="row">
+
+                    <div class="small-12 large-12 columns panel top">
+                        <h1>Main Area</h1>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="small-1 large-1 columns">
+                        <!-- rien du tout -->
+                    </div>
+                    <div class="article small-10 large-10 columns">
+                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <div class="panel article">
+                            <h1><?php the_title(); ?></h1>
+                            <h4>Posted on <?php the_time('F jS, Y') ?></h4>
+                            <?php the_content(__('(more...)')); ?>
+                        </div>
+
+                    </div>
+
+                    <?php endwhile; else: ?>
+
+                    <div class="panel article">
+                        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+                    </div>
+
+                    <div class="small-1 large-1 columns">
+                        <!-- rien du tout -->
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!--
@@ -48,7 +60,36 @@
 
         
     </div>
-</div>
-<div id="delimiter">
-</div>
-<?php get_footer(); ?>
+
+    <!--
+
+    <div id="delimiter">
+    </div>
+
+-->
+
+    <div id="footer">
+        <?php include 'footer.php'; ?>
+    </div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
